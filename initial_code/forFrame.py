6d4bdc7a3561da1e825168ce702a802120244133
park8989 <41559189+park8989@users.noreply.github.com>
2018-10-02 06:00:12 +0000

    Create forFrame.py
    Various "for" statements

---
+++ initial_code/forFrame.py
@@ -0,0 +1,14 @@
+for element in ['月', '火', '水', '木', '金', '土', '日']: #リストのfor文
+    print(element, end=" ") #printの時、改行せず、スペース開けて表示
+
+print()
+for element in ('月', '火', '水', '木', '金', '土', '日'): #tupleのfor文
+   print(element, end=" ") #printの時、改行せず、スペース開けて表示
+print()
+for character in 'あいうえお': # 文字列for文
+   print(character)
+   
+for i in range(10): # range()のfor文
+   print(i, end=" ")
+   
+print()
