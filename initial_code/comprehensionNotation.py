8715b25765557bde612e55008d7c11271c9f8d72
park8989 <41559189+park8989@users.noreply.github.com>
2018-10-18 13:12:44 +0900

    Create comprehensionNotation.py
    Python's comprehension notation

---
+++ initial_code/comprehensionNotation.py
@@ -0,0 +1,34 @@
+#! python3
+# Python's comprehension notation
+
+print([i for i in range(0, 100)])    　　                             # 0-99をリスト化
+print([0 for i in range(0, 100)])   　　                              # 0を100個リスト化
+print([i % 2 for i in range(0, 100)])                                 # 0,1,0,1 が繰り返される
+
+print([i for i in range(0, 100) if i % 3 == 0])
+x3 = []                                                               # 3の倍数
+x3.extend([i for i in range(0, 100) if i % 3 == 0])
+print(x3, len(x3))
+
+import os, glob, time
+
+[f for f in glob.glob("*") if os.stat(f).st_mtime > time.time() - 60*60*24 ]
+					　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　#time.time()で現在時刻をエポック秒で取得する。
+					　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　#glob.glob("*")でカレントディレクトリのファイルの一覧を列挙
+
+
+# 入れ子構造にすることも可能
+print([(x, y) for x in range(3) for y in range(3)])
+#[(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+# 九九の表を作る
+print([(x * y) for x in range(1, 10) for y in range(1, 10)])
+# [1, 2, 3, 4, 5,~~~~~ 54, 63, 72, 81]
+
+
+kuku = [(x,y,x*y) for x in range(1, 10) for y in range(1,10)]
+st = ''
+for x,y,z in kuku:
+    st += '{}*{}={} '.format(x,y,z)
+    if y == 9: st += "\n"　　　　　　　　　　　　　　　　　　　　　　　　# 9で改行する
+print(st)
